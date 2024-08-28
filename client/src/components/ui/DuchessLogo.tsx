fa50c4304a70cf0a88e73bc96287c15c7daf3920
yegor <astery227@gmail.com>
2024-08-28 15:10:00 +0300

    refac: move duchess logo to its own component

---
+++ client/src/components/ui/DuchessLogo.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function DuchessLogo() {
+    return (
+        <Link to="/" className="text-4xl font-thin text-center p-5">
+            DUCHESS FITNESS
+        </Link>
+    );
+}
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,13 @@
-import { Link } from "react-router-dom";
 import HomePageFirstPart from "@/pages/HomePage/components/HomePageFirstPart.tsx";
 import HomePageBenefits from "@/pages/HomePage/components/HomePageBenefits.tsx";
 import HomePageMap from "@/pages/HomePage/components/HomePageMap.tsx";
 import HomePageSubscriptions from "@/pages/HomePage/components/HomePageSubscriptions/HomePageSubscriptions.tsx";
+import DuchessLogo from "@/components/ui/DuchessLogo.tsx";
 
 export default function HomePage() {
     return (
         <div className="flex flex-col bg-default h-full">
-            <Link to="/" className="text-4xl font-thin text-center p-5">
-                DUCHESS FITNESS
-            </Link>
+            <DuchessLogo />
             <HomePageFirstPart />
             <HomePageBenefits />
             <HomePageMap />
