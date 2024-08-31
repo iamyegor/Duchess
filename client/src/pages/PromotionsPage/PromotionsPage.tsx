bc5970500c2af9e7742e39d78ecee6ce86ab6dd9
yegor <astery227@gmail.com>
2024-08-31 14:46:28 +0300

    feat: change the picture on the promotions page

--- client/src/pages/PromotionsPage/PromotionsPage.tsx
+++ client/src/pages/PromotionsPage/PromotionsPage.tsx
@@ -1,5 +1,5 @@
 import DuchessLogo from "@/components/ui/DuchessLogo";
-import promotionsImage from "@/assets/promotionsPage/promotions.jpg";
+import promotionImage from "@/assets/promotionsPage/promotion.png";
 
 function PromotionsPage() {
     return (
@@ -7,17 +7,20 @@ function PromotionsPage() {
             <DuchessLogo />
             <div className="container h-full space-y-10 flex flex-col">
                 <h1 className="text-5xl font-semibold">АКЦИИ</h1>
-                <div className="bg-black text-white rounded-xl flex h-auto md:h-[400px] lg:h-[500px] flex-col md:flex-row">
-                    <div className="flex-none md:flex-1 h-[500px] md:h-full">
+                <div className="bg-black text-white rounded-xl flex h-auto md:h-[350px] lg:h-[500px] flex-col md:flex-row">
+                    <div
+                        className="flex-none "
+                        style={{ aspectRatio: "1 / 1" }}
+                    >
                         <img
-                            src={promotionsImage}
+                            src={promotionImage}
                             alt="Promotion Image"
                             className="mr-8 w-full h-full rounded-t-xl md:rounded-tr-none md:rounded-l-xl object-cover"
                         />
                     </div>
-                    <div className="flex flex-1 flex-col justify-between p-8 space-y-4">
+                    <div className="flex flex-1 flex-col justify-between p-6 lg:p-8 space-y-4">
                         <div>
-                            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4">
+                            <h1 className="text-3xl lg:text-5xl font-bold mb-4">
                                 0₽ на вступительный взнос
                             </h1>
                             <p className="text-xl lg:text-2xl mb-6">
@@ -25,10 +28,10 @@ function PromotionsPage() {
                                 клубах сети Duchess Fitness.
                             </p>
                         </div>
-                        <div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row justify-between items-center">
-                            <span className="text-sm lg:text-base">19 АВГУСТА 2024</span>
+                        <div className="flex flex-col space-y-4 lg:space-y-0 lg:flex-row justify-between items-center">
+                            <span className="text-base">Финальные скидки</span>
                             <button
-                                className="bg-default hover:bg-default-dark text-black font-semibold py-2 px-5 rounded-lg w-full sm:w-auto"
+                                className="bg-default hover:bg-default-dark text-black font-semibold py-2 px-7 rounded-lg w-full lg:w-auto"
                                 onClick={() => alert("Абонемент куплен!")}
                             >
                                 КУПИТЬ АБОНЕМЕНТ
